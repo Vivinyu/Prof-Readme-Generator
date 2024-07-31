acf950c92474bfd0aaf9f1fdbe09b485fd93bd96
Vivinyu <josedelossantoso@gmail.com>
2024-07-31 19:58:57 -0400

    added all code and tested application

--- README.md
+++ README.md
@@ -1,1 +1,34 @@
-# Prof-Readme-Generator
+
+  # Professional Readme.md Generator
+  
+  ## Description
+  This program will generate a readme.md file for all your projects
+  
+  ## Table of Contents
+  - [Installation](#installation)
+  - [Usage](#usage)
+  - [License](#license)
+  - [Contributing](#contributing)
+  - [Tests](#tests)
+  - [Questions](#questions)
+  
+  ## Installation
+  copy the index.js file to your file structure, run the program by typing node index.js in the terminal
+  
+  ## Usage
+  Use it everytime you create a repo and want to give a description of what the project is about
+  
+  ## License
+  This project is covered under the MIT license.
+  
+  ## Contributing
+  no contributions needed for this project.
+  
+  ## Tests
+  You can test this app by coping the necessary files in this repo.
+  
+  ## Questions
+  For additional questions, contact me:
+  GitHub: [Vivinyu](https://github.com/Vivinyu)
+  Email: josedelossantos@me.com
+  
--- index.js
+++ index.js
@@ -1,2 +1,103 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
+
+const questions = [
+    {
+      type: 'input',
+      name: 'title',
+      message: 'What is the title of your project?',
+    },
+    {
+      type: 'input',
+      name: 'description',
+      message: 'Provide a description of your project:',
+    },
+    {
+      type: 'input',
+      name: 'installation',
+      message: 'Provide installation instructions:',
+    },
+    {
+      type: 'input',
+      name: 'usage',
+      message: 'Provide usage information:',
+    },
+    {
+      type: 'list',
+      name: 'license',
+      message: 'Choose a license for your project:',
+      choices: ['MIT', 'Apache 2.0', 'GPL 3.0', 'BSD 3', 'None'],
+    },
+    {
+      type: 'input',
+      name: 'contributing',
+      message: 'Provide contribution guidelines:',
+    },
+    {
+      type: 'input',
+      name: 'tests',
+      message: 'Provide test instructions:',
+    },
+    {
+      type: 'input',
+      name: 'github',
+      message: 'Enter your GitHub username:',
+    },
+    {
+      type: 'input',
+      name: 'email',
+      message: 'Enter your email address:',
+    },
+  ];
+
+  function generateREADME(answers) {
+    return `
+  # ${answers.title}
+  
+  ## Description
+  ${answers.description}
+  
+  ## Table of Contents
+  - [Installation](#installation)
+  - [Usage](#usage)
+  - [License](#license)
+  - [Contributing](#contributing)
+  - [Tests](#tests)
+  - [Questions](#questions)
+  
+  ## Installation
+  ${answers.installation}
+  
+  ## Usage
+  ${answers.usage}
+  
+  ## License
+  This project is covered under the ${answers.license} license.
+  
+  ## Contributing
+  ${answers.contributing}
+  
+  ## Tests
+  ${answers.tests}
+  
+  ## Questions
+  For additional questions, contact me:
+  GitHub: [${answers.github}](https://github.com/${answers.github})
+  Email: ${answers.email}
+  `;
+  }
+
+  function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, (err) =>
+      err ? console.error(err) : console.log('README.md generated successfully!')
+    );
+  }
+
+  function init() {
+    inquirer.prompt(questions).then((answers) => {
+      const readmeContent = generateREADME(answers);
+      writeToFile('README.md', readmeContent);
+    });
+  }
+
+  init();
